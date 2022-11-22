e534aed691ce07a4af36e2460f0c87c596aaf64e
irish15 <irishx15@gmail.com>
2022-11-22 17:50:15 +0900

    update env

--- lib/firebase.tsx
+++ lib/firebase.tsx
@@ -14,16 +14,23 @@ import { initializeApp } from "firebase/app";
 
 export default async function initFirebase() {
     // const serviceAccount = require('./serviceAccountKey.json');
-    var serviceAccount = require('./serviceAccountKey.json');
     const firebaseConfigs = {
-        apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-        authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-        projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-        storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-        messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-        appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-        measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMNT_ID,
-        credential: serviceAccount,
+        apiKey: process.env.FIREBASE_API_KEY,
+        authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+        messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+        appId: process.env.FIREBASE_APP_ID,
+        measurementId: process.env.FIREBASE_MEASUREMNT_ID,
+        // credential: {
+        //     type: process.env.TYPE,
+        //     project_id: PROJECT_ID,
+        //     private_key_id: ,
+        //     private_key: ,
+        //     client_email
+        //     client_id,
+        //     auth
+        // },
     }
     // console.log('firebaseConfig: \n', firebaseConfigs)
     initializeApp(firebaseConfigs);
