0b64cb958eb51a036b1f382204aa9730228c39bf
irish15 <irishx15@gmail.com>
2022-11-22 18:43:37 +0900

    update env

--- lib/firebase.tsx
+++ lib/firebase.tsx
@@ -13,28 +13,28 @@ import { initializeApp } from "firebase/app";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
 export default async function initFirebase() {
-    console.log('bucket: ', process.env.FIREBASE_STORAGE_BUCKET)
+    console.log('bucket: ', process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET)
     // const serviceAccount = require('./serviceAccountKey.json');
     const firebaseConfigs = {
-        apiKey: process.env.FIREBASE_API_KEY,
-        authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-        projectId: process.env.FIREBASE_PROJECT_ID,
-        storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-        messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-        appId: process.env.FIREBASE_APP_ID,
-        measurementId: process.env.FIREBASE_MEASUREMNT_ID,
-        credential: {
-            type: process.env.TYPE,
-            project_id: process.env.PROJECT_ID,
-            private_key_id: process.env.PRIVATE_KEY_ID,
-            private_key: process.env.PRIVATE_KEY,
-            client_email: process.env.CLIENT_EMAIL,
-            client_id: process.env.CLIENT_ID,
-            auth_uri: process.env.AUTH_URI,
-            token_uri: process.env.TOKEN_URI,
-            auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_X509_CERT_URL,
-            client_x509_cert_url: process.env.CLIENT_X509_CERT_URL,
-        },
+        apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+        authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+        projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+        storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+        messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+        appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+        measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMNT_ID,
+        // credential: {
+        //     type: process.env.TYPE,
+        //     project_id: process.env.PROJECT_ID,
+        //     private_key_id: process.env.PRIVATE_KEY_ID,
+        //     private_key: process.env.PRIVATE_KEY,
+        //     client_email: process.env.CLIENT_EMAIL,
+        //     client_id: process.env.CLIENT_ID,
+        //     auth_uri: process.env.AUTH_URI,
+        //     token_uri: process.env.TOKEN_URI,
+        //     auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_X509_CERT_URL,
+        //     client_x509_cert_url: process.env.CLIENT_X509_CERT_URL,
+        // },
     }
     // console.log('firebaseConfig: process.env.\n', firebaseConfigs)
     initializeApp(firebaseConfigs);
