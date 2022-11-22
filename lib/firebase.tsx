152a96ad6520cd6bdd43c1b66c130c102dbb984d
irish15 <irishx15@gmail.com>
2022-11-22 17:57:29 +0900

    add credentials

--- lib/firebase.tsx
+++ lib/firebase.tsx
@@ -22,23 +22,26 @@ export default async function initFirebase() {
         messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
         appId: process.env.FIREBASE_APP_ID,
         measurementId: process.env.FIREBASE_MEASUREMNT_ID,
-        // credential: {
-        //     type: process.env.TYPE,
-        //     project_id: PROJECT_ID,
-        //     private_key_id: ,
-        //     private_key: ,
-        //     client_email
-        //     client_id,
-        //     auth
-        // },
+        credential: {
+            type: process.env.TYPE,
+            project_id: process.env.PROJECT_ID,
+            private_key_id: process.env.PRIVATE_KEY_ID,
+            private_key: process.env.PRIVATE_KEY,
+            client_email: process.env.CLIENT_EMAIL,
+            client_id: process.env.CLIENT_ID,
+            auth_uri: process.env.AUTH_URI,
+            token_uri: process.env.TOKEN_URI,
+            auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_X509_CERT_URL,
+            client_x509_cert_url: process.env.CLIENT_X509_CERT_URL,
+        },
     }
-    // console.log('firebaseConfig: \n', firebaseConfigs)
+    // console.log('firebaseConfig: process.env.\n', firebaseConfigs)
     initializeApp(firebaseConfigs);
 
     // const admin = require("firebase-admin");
     // const serviceAccount = require("./serviceAccountKey.json");
     // admin.initializeApp({
-    //     credential: admin.credential.cert(serviceAccount)
+    //     credential: process.env.admin.credential.cert(serviceAccount)
     // });
 
 
